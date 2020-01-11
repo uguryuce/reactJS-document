d1eb5b17d594f938a867112a0884fb5382b081e5
uguryuce <uguryucae1903@gmail.com>
2020-01-11 16:46:17 +0300

    context-api 1

---
+++ src/context.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+const UserContext = React.createContext();
+//Provider ,Consumer
+
+export class UserProvider extends Component {
+    state = {
+        users : [
+          {
+            id : 1,
+            name : "Uğur Yüce",
+            salary : "5000",
+            department : "Bilişim"
+          },
+          {
+            id : 2,
+            name : "İlteriş Keskin",
+            salary : "4000",
+            department : "Yazılım"
+          },
+          {
+            id : 3,
+            name : "Ugi",
+            salary : "7000",
+            department : "Üretim"
+          }
+        ]
+      }
+
+    render() {
+        return (
+            <UserContext.Provider value = {this.state}>
+                {this.props.children}
+            </UserContext.Provider>
+        )
+    }
+}
+
+const UserConsumer = UserContext.Consumer;
+
+export default UserConsumer;
