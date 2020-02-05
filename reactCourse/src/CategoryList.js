42a01d018bb54888130b3926b288d9b80aa22e82
uguryuce <uguryucae1903@gmail.com>
2020-02-05 17:41:38 +0300

    state

--- reactCourse/src/CategoryList.js
+++ reactCourse/src/CategoryList.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import {ListGroup, ListGroupItem} from 'reactstrap';
 
 export default class CategoryList extends Component {
+    
+        
+        state = {
+            categories : [
+                {categoryID: 1, categoryName : "Beverages"},
+                {categoryID: 2, categoryName : "Condiments"}
+            ]
+        };
+    
 
     
     render() {
@@ -9,11 +18,13 @@ export default class CategoryList extends Component {
             <div>
                 <h3>{this.props.info.title}</h3>
                 <ListGroup>
-                    <ListGroupItem>lorem1</ListGroupItem>
-                    <ListGroupItem>lorem2</ListGroupItem>
-                    <ListGroupItem>lorem3</ListGroupItem>
-                    <ListGroupItem>lorem4</ListGroupItem>
-                    <ListGroupItem>lorem5</ListGroupItem>
+                    {
+                        this.state.categories.map(category => (
+                            <ListGroupItem key = {category.categoryID}>{category.categoryName}</ListGroupItem>
+
+                        ))
+                    }
+                    
                 </ListGroup>
             </div>
         )
