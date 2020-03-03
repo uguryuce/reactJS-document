30548817e319417cf1797a69416baad2f714afde
uguryuce <uguryucae1903@gmail.com>
2020-03-03 22:22:16 +0300

    build

---
+++ reactapp1/build/precache-manifest.1df8f0b56f2c4d19d3ac3afdc1f3a077.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7028ca23eb6de64326f3b87b76da7248",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b899b6e0b0cc985008c",
+    "url": "/static/css/main.533b942e.chunk.css"
+  },
+  {
+    "revision": "15876a95c342433db86e",
+    "url": "/static/js/2.d8fb97f2.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.d8fb97f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b899b6e0b0cc985008c",
+    "url": "/static/js/main.db0d971d.chunk.js"
+  },
+  {
+    "revision": "a6fe05a643fb07ae6fa5",
+    "url": "/static/js/runtime-main.b4c33d91.js"
+  }
+]);
